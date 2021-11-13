0ba5ca193e4bb61dfac30866cb6432168730fc1b
ShahriarRaktim <shahriarahmedraktim@gmail.com>
2021-11-13 11:53:09 +0600

    added

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0js5x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("Glass");
+    const productsCollection = database.collection("products");
+    const reviewCollection = database.collection("review");
+    const orderedCollection = database.collection("odered");
+    const usersCollection = database.collection("user");
+
+    //Get All
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    //Get All
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    //Get One
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const service = { _id: ObjectId(id) };
+      const result = await productsCollection.findOne(service);
+      console.log(result);
+      res.send(result);
+    });
+    //Post
+    app.post("/review", async (req, res) => {
+      const service = req.body;
+      const result = await reviewCollection.insertOne(service);
+      res.json(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    //get
+    app.get("/ordered", async (req, res) => {
+      const email = req.query.email;
+      const query = { useremail: email };
+      const cursor = orderedCollection.find(query);
+      const ordered = await cursor.toArray();
+      res.json(ordered);
+    });
+
+    //get admin
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    //put
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    //Delete
+    app.delete("/ordered/:id", async (req, res) => {
+      const id = req.params.id;
+      const service = { _id: ObjectId(id) };
+      const result = await orderedCollection.deleteOne(service);
+      res.send(result);
+    });
+    //put make admin
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Glasses Server");
+});
+
+app.listen(port, () => {
+  console.log("Running Glass Server on port", port);
+});
